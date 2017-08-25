7237b98ac9ce115a1dc67a0360f31052bf55c287
paula-arantes <paula.arantes@cognitivo.ai>
2017-08-25 13:11:09 -0300

    adicionando o parametro de email a xablauniano

--- xablauniano.py
+++ xablauniano.py
@@ -6,9 +6,10 @@ class Xablauniano():
     def __init__(self, lista):
         self.name =  lista[0]
         self.profession = lista[1]
+        self.email = lista[2]
         self.summary = ''.join(lista[lista.index('Summary') +1: lista.index('Experience')])
         self.experience = parse_experience(lista[lista.index('Experience') +1: lista.index('Education')])
-        self.education = lista[lista.index('Education') +1]
+        self.education = lista[lista.index('Education') +1: ]
     
 
 
